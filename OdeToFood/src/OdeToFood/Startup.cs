a1e9fa6e6a23fb7b496035713e598da1791e2750
cmckinnie <christopher.mckinnie@rxaap.com>
2016-10-27 12:08:01 -0500

    Middleware configured

--- OdeToFood/src/OdeToFood/Startup.cs
+++ OdeToFood/src/OdeToFood/Startup.cs
@@ -21,15 +21,16 @@ namespace OdeToFood
             builder
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables();  
+                .AddEnvironmentVariables();
 
-            Configuration = builder.Build(); 
+            Configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
             services.AddSingleton(Configuration);
             services.AddSingleton<IGreeter, Greeter>();
         }
@@ -45,14 +46,18 @@ namespace OdeToFood
 
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+               app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(new ExceptionHandlerOptions {
+                    ExceptionHandler = context => context.Response.WriteAsync("Opps!")
+                });
             }
 
-            app.Run(async (context) =>
-            {
-                var message = greeter.GetGreeting();
-                await context.Response.WriteAsync(message);
-            });
+            app.UseFileServer();
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
