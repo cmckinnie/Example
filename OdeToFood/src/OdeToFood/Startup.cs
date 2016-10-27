1c79d428518089a7ae46dc42bead2d3d4a8476b5
cmckinnie <christopher.mckinnie@rxaap.com>
2016-10-27 11:13:09 -0500

    Change 2

--- OdeToFood/src/OdeToFood/Startup.cs
+++ OdeToFood/src/OdeToFood/Startup.cs
@@ -23,7 +23,7 @@ namespace OdeToFood
                 .AddJsonFile("appsettings.json")
                 .AddEnvironmentVariables();  
 
-            Configuration = builder.Build();
+            Configuration = builder.Build(); 
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
